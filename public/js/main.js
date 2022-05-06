bb0075dfd721057560c1e8bca4639849881be960
enzo grillo <enzogrillo96@gmail.com>
2022-05-06 14:50:59 +0200

    first

---
+++ public/js/main.js
@@ -0,0 +1,148 @@
+// part1
+// let num1 = prompt("gimme a number")
+// let num2 = prompt("gimme a number")
+// let num3 = num1 === num2
+
+// console.log(num1, num2)
+
+// alert(num1 === num2)
+
+// alert("le nombre " + num1 + " et le nombre " + num2 + " sont ils egaux ? " + num3  )
+
+
+
+// part2
+// let num1 = prompt("gimme a number")
+// let num2 = prompt("gimme a number")
+
+// console.log(num1, num2)
+
+// alert(num1 < num2)
+
+// let num3 = num1 < num2
+
+// alert("le nombre " + num1 + " et-il plus petit que le nombre " + num2 + "?"  + num3  )
+
+
+// part3
+// let num1 = prompt("gimme a number")
+// let num2 = prompt("gimme a number")
+// let num3 = prompt("gimme a number")
+// let add = (num1 + num2 > num3)
+
+// alert("La somme du nombre " + num1 + " + le nombre "+ num2 + " est-elle plus grande que le nombre "+ num3 +  "? : reponse " + add)
+
+
+// part4
+// let question = prompt("gimme a phrase")
+// let longueur = prompt("Estimation longueur")
+// let answer = question.length - longueur
+// console.log(question.length)
+
+// if (question.length == longueur){
+//     alert("Bonne reponse")
+// }else{
+//     alert("Incorrect tu étais à " + answer + " unités de la bonne réponse !")
+
+// }
+
+// part5
+// let question = prompt("Enter name !")
+
+// if (question === "") {
+//     prompt("Attention vous devez remplir le champs ci dessous, Quel est votre prènom ?")
+
+// } else {
+//     alert("Bonjour " + question)
+// }
+
+
+// part6
+// let demande = confirm("Do you want to subscribe ?");
+
+// if (demande) {
+//     let question = prompt("Would you like the normal or deluxe package ?")  
+//     if (question == "normal") {
+//         alert("Merci pour votre abonnement.")
+//     }else if (question == "deluxe") {
+//         alert("Félicitation vous avez choisis la formule Luxe !")
+//     }else if (question == "") {
+//         let demande = confirm("Are you sure ?");
+//         if (demande){
+//             alert("C'est dommage bonne journée")
+//         }else {
+//             let demande = confirm("Would you like the normal or deluxe package ?");
+//         }
+//     }
+// } else {
+//     alert("good day")
+// }
+
+// part7
+// let question1 = prompt("2 + 2 ?")
+// let question2 = prompt("Quelle est la couleur du cheval rouge de yves ?")
+
+// let answer1 = 4
+// let answer2 = "rouge"
+
+// if (question1 == answer1, question2 == answer2) {
+//     alert("yeppaaaaaa")
+
+// }else {
+//     alert("Nonono")
+
+// } if (question1 == "", question2 == answer2){
+//     alert("1-non 2-oui")
+
+// }else if (question1 == answer1, question2 == ""){
+//     alert("1-oui 2-non")
+// } 
+
+// part8
+// let cinema = prompt("How old are you ?")
+
+// if (cinema == 0 ) {
+//     alert("vous n’êtes même pas nés, n’essayez pas de tricher")
+// }else if (cinema < 18 ) {
+//     alert("vous êtes mineur, l’accès est réservé aux grands")
+// }else if (cinema > 18 && cinema < 100) {
+//     alert("vous êtes majeur, vous pouvez réserver")
+// }else if (cinema > 100) {
+//     alert("vous êtes encore vivant ? Évitez de voir ce film pour le rester.")
+    
+// }else if (cinema == 18)
+//     alert("pouvez rentrer mais c’est tout juste, n'oubliez pas que ce film peut être choquant.")
+    
+
+
+// part9
+
+// let riche = confirm("est-ce que vous êtes riche ?");
+// let vacance = confirm("voulez-vous partir en vacance ?");
+// let chat = confirm("est-ce que vous avez un chat ?" );
+
+// if (riche == true && vacance == true && chat == false) {
+//     alert("Tout est parfait, partez en vacance !");
+// } else if (riche == true && vacance == false && chat == false || riche == true && vacance == false && chat == true ) {
+//     alert("pas de problème, ne partez pas en vacance");
+// } else if (riche == false || chat == true) {
+//     alert("Meme si vous le voulez, vous ne pouvez pas partir.");
+// } else if (riche == true && chat == false) {
+//     alert("Vous pourriez partie en vacance si vous le voulez")
+// }
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
